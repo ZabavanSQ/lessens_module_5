364243523554fa660f60fe44c831351dbb638047
ZabavanSQ <zabavnsq@mail.ru>
2024-12-03 15:18:42 +0300

    Атрибуты и методы дороботка

--- module_5.1.py
+++ module_5.1.py
@@ -1,6 +1,6 @@
 class House:
     def __init__(self, name, number_of_floor):
-        self.mame = name
+        self.name = name
         self.number_of_floor = number_of_floor
 
     def go_to(self, new_floor):
@@ -10,12 +10,14 @@ class House:
             else:
                 print("Такого этажа не существует!")
                 break
+        if new_floor <= 0:
+            print("Такого этажа не существует!")
             pass
 
 h1 = House('ЖК Горский', 18)
 h2 = House('домик в деревне', 2)
 h3 = House('ЖК Новые ватутинки', 17)
 h1.go_to(5)
-h2.go_to(10)
-h3.go_to(2)
+h2.go_to(11)
+h3.go_to(-12)
 
